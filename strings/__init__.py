f494157159a80fe4a783e6dbee8f274ad80fceed
Blackshinigaami <106493387+blackshinigaami@users.noreply.github.com>
2022-09-13 01:51:38 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,12 +1,9 @@
 #
-# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# Copyright (C) 2021-2022 by @Cheemsland, @TheYukki, @YukkiSupport
 
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. © Alisha © Alexa © Yukki
+# Kanged By © Cheemsland
+# Owner Cheems
+# All rights reserved.  © Yukki
 
 
 import os
